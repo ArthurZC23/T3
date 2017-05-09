18438d37664887d2e90f011a8568fd04af01c001
Arthur Zanatta da Costa <arthurzc23@gmail.com>
2017-05-09 18:39:05 -0300

    Added center and origianl zoom button

--- project/web_page/js/initialization.js
+++ project/web_page/js/initialization.js
@@ -1,28 +1,36 @@
+var map;
 var safetyCircle;
+var sanFrancisco = {lat: 37.773972, lng: -122.431297};
 
 function myMap() {
 
-  var latLng = new google.maps.LatLng(37.773972,-122.431297)
+  var latLng = new google.maps.LatLng(sanFrancisco)
   var mapOptions = {
     center: latLng,
-    zoom: 12,
+    zoom: 13,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   }
 
-  var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+  map = new google.maps.Map(document.getElementById("map"), mapOptions);
   var marker = new google.maps.Marker({
     position: latLng,
     map: map
 
     });
-  safetyCircle = new google.maps.Circle({
-   strokeColor: '#FF0000',
-   strokeOpacity: 0.8,
-   strokeWeight: 2,
-   fillColor: '#FF0000',
-   fillOpacity: 0.35,
-   map: map,
-   center: latLng,
-   radius: 1 * 1000
-  });
+
+var centerControlDiv = document.createElement('div');
+var centerControl = new CenterControl(centerControlDiv, map);
+centerControlDiv.index = 1;
+map.controls[google.maps.ControlPosition.TOP_CENTER].push(centerControlDiv);
+
+safetyCircle = new google.maps.Circle({
+strokeColor: '#FF0000',
+strokeOpacity: 0.8,
+strokeWeight: 2,
+fillColor: '#FF0000',
+fillOpacity: 0.35,
+map: map,
+center: latLng,
+radius: 1 * 1000
+});
 }
