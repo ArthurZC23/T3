d0a7f64087cea14603187d938de9539a46dde6db
Arthur Zanatta da Costa <arthurzc23@gmail.com>
2017-06-02 15:58:14 -0300

    Modularize the app and removed js position arrays

--- project/js/scripts.js
+++ project/js/scripts.js
@@ -184,8 +184,6 @@ function barChart(relevantCrimes){
       .attr("height", function(d) { return height - y(d.Number); });
 }
 
-
-
 function style_circle(dangerLevel){
 
   if (dangerLevel <= 1000){
@@ -242,3 +240,39 @@ $.getJSON("https://raw.githubusercontent.com/ArthurZC23/T3/master/project/data/P
   }
   visualizeCrime(null);
 });
+
+function updateRadius(circle, radius){
+
+  circle.set('radius', parseInt(radius, 10));
+}
+
+function CenterControl(controlDiv, map) {
+
+  // Set CSS for the control border.
+  var controlUI = document.createElement('div');
+  controlUI.style.backgroundColor = '#fff';
+  controlUI.style.border = '2px solid #fff';
+  controlUI.style.borderRadius = '3px';
+  controlUI.style.boxShadow = '0 2px 6px rgba(0,0,0,.3)';
+  controlUI.style.cursor = 'pointer';
+  controlUI.style.marginBottom = '22px';
+  controlUI.style.textAlign = 'center';
+  controlUI.title = 'Click to recenter the map  in SF';
+  controlDiv.appendChild(controlUI);
+
+  // Set CSS for the control interior.
+  var controlText = document.createElement('div');
+  controlText.style.color = 'rgb(25,25,25)';
+  controlText.style.fontFamily = 'Roboto,Arial,sans-serif';
+  controlText.style.fontSize = '16px';
+  controlText.style.lineHeight = '38px';
+  controlText.style.paddingLeft = '5px';
+  controlText.style.paddingRight = '5px';
+  controlText.innerHTML = 'Center Map in SF';
+  controlUI.appendChild(controlText);
+
+  controlUI.addEventListener('click', function() {
+    map.setCenter(sanFrancisco);
+    map.setZoom(13)
+  });
+}
